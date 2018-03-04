694e0ea91a182b77f6d3b633853796fb5d77c659
AvichalChadha <avichalchadha1999@gmailcom>
2018-03-05 02:45:59 +0530

    validating the forms and error messages done!

--- index.py
+++ index.py
@@ -1,22 +1,18 @@
 
 '''
 thinhs to be done- 
-1) the validation lenght is to be  set ptoparly. Not done yet for testing purposes! 
 2) Even the sql table varchar has to be set! 
-3) emial and URL validation is not also set 
 5) THere are couple of pics clicked on the phone that tells how to give your sql location whn you are hosting it on the server. 
 or go to the flask blog at 24:02, or just copy from it's github code. 
 in the post_submitted section, write this- written by _____ on _____
-6) So after validation the users puts the wrong email, it will show some message like please put the valid email address. 
 7) recaptcha before posting the blog post
-8) In validators do mention that if you don't have your own url then don't make it validator.required
 '''
 
 
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from wtforms import Form, BooleanField, StringField, PasswordField, validators, validators, ValidationError, TextAreaField, TextField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Email, Length, URL
 from flask_wtf import Form
 from flask_wtf import FlaskForm
 
@@ -52,13 +48,13 @@ class posts(db.Model):
 
 class Post_Form(Form):
     #link = TextField('Link', [validators.Length(min=3, max=20), validators.URL()])
-    title = TextField('Title', [validators.Length(min=1, max=160)])
-    url= TextField("Website's Link", [validators.Length(min=1, max=160)])
-    author = TextField("Author's Name", [validators.Length(min=1, max=120)]) # name of the author
-    description = TextField('Subtitle', [validators.Length(min=1, max=160)])
-    post = TextAreaField('Post', [validators.Length(min=1, max=3000)], render_kw={"rows": 30, "cols": 50})
-    AuthorDescription = TextAreaField("Author's Description", [validators.Length(min=1, max=600)], render_kw={"rows": 5, "cols": 50})
-    email = TextField("Email Address", [validators.Length(min=1, max=120)]) 
+    title = TextField('Title',validators=[InputRequired(), Length(min=5, max=60, message="The field must be between 5 and 60 characters long")],render_kw={"placeholder": "Title of your Post"})
+    url= TextField("Website's Link",render_kw={"placeholder": "Your Blog's Link If You Have Any"})
+    author = TextField("Author's Name",validators=[InputRequired(), Length(min=3, max=40, message="The field must be between 5 and 40 characters long")],render_kw={"placeholder": "Author's Name"}) # name of the author
+    description = TextField('Subtitle', validators=[InputRequired(), Length(min=5, max=400, message="The field must be between 5 and 380 characters long")] ,render_kw={"placeholder": "Subtitle Of Your Post"})
+    post = TextAreaField('Post', validators=[InputRequired(), Length(min=5, max=5000, message="The field must be between 5 and 2000 characters long")], render_kw={"rows": 30, "cols": 50})
+    AuthorDescription = TextAreaField("Author's Description",validators=[InputRequired(), Length(min=30, max=1000, message="The field must be between 5 and 60 characters long")], render_kw={"rows": 5, "cols": 50})
+    email = TextField("Email Address",validators=[InputRequired(),Email(message='Enter a valid email'), Length(min=10, max=100)],render_kw={"placeholder": "Your Email Address"}) 
     
 
     
@@ -81,9 +77,10 @@ def index():
             Data = posts(title= form.title.data, url= form.url.data, author=form.author.data, description=form.description.data,post=form.post.data,AuthorDescription=form.AuthorDescription.data,email=form.email.data, date_posted = postedDate, random=url_genrator)
             db.session.add(Data)
             db.session.commit()
-            return "Done LOL"
+
+            return redirect(url_for('after_submissions'))
         else : 
-            return "i don't know how to react"
+            return render_template("post.html",form=form)
 
 ##just to clarify few thing, in this code-  'lol = posts(title= form.title.data, url= form.url.data, author=form.author.data, description=form.description.data,post=form.post.data,AuthorDescription=form.AuthorDescription.data,email=form.email.data)'
 ## 'title' in the left is reffered to the 'title' in the class 'posts', i.e the one resposnible for SQLdb. 
@@ -108,6 +105,9 @@ def guest_post(random_id):
     
 
 
+@app.route('/submitted_posts/')
+def after_submissions():
+    return render_template ('submission.html')
 
 
 
